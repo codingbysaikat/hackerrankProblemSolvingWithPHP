db65b7286787a764e56a5d268ea61884578d121f
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-08-27 01:29:22 +0600

    updated

--- solveMeFirst.php
+++ solveMeFirst.php
@@ -1,11 +1,31 @@
-function solveMeFirst($a,$b){
-  // Hint: Type return $a + $b; below  
-  
-}
+<!-- Complete the function solveMeFirst to compute the sum of two integers.
 
-$handle = fopen ("php://stdin","r");
-$_a = fgets($handle);
-$_b = fgets($handle);
-$sum = solveMeFirst((int)$_a,(int)$_b);
-print ($sum);
-fclose($handle);
+Example
+a =7;
+b = 3;
+Return 10.
+
+Function Description
+Complete the solveMeFirst function in the editor below.
+solveMeFirst has the following parameters:
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  and 
+
+Constraints
+1=< a,b =<100;
+
+Sample Input:
+a = 2;
+b = 3;
+Sample Output:
+5; -->
+<?php
+
+function solveMeFirst($a,$b){
+$c=$a+$b;
+return $c;
+}
+echo solveMeFirst(100,1000);
+?>
